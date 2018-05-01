a5b3b26c763126200cc85038b5f68246d7830562
Claire Tran <c.tran83@gmail.com>
2018-05-01 21:35:58 +1000

    adding in sockets

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,8 +1,10 @@
 defmodule Discuss.CommentsChannel do
 	use Discuss.Web, :channel
 
-	def join() do
-		
+	def join(name, _params, socket) do
+    IO.puts("-------------------")
+		IO.puts(name)
+    {:ok, %{}, socket}
 	end
 
 	def handle_in() do
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -55,7 +55,7 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect() //connects to backend server
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {}) //default channel
+let channel = socket.channel("comments:1", {}) //default channel
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
